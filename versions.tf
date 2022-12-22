fbce980ce7df9d668b331e5d96aacfed89a9b66d
Ihor Naidonov <superihor20@gmail.com>
2022-12-22 13:49:21 +0200

    add providers

---
+++ versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.48.0"
+    }
+  }
+
+  required_version = "1.3.6"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
