c45b223d9746513e0a0b6171c4a6500da2411c88
Ihor Naidonov <superihor20@gmail.com>
2022-12-23 10:38:43 +0200

    added validation for vpc_id

--- modules/internet-gateway/variables.tf
+++ modules/internet-gateway/variables.tf
@@ -1,4 +1,9 @@
 variable "vpc_id" {
   description = "The ID of the VPC"
   type        = string
+
+  validation {
+    condition     = can(regex("^vpc-", var.vpc_id))
+    error_message = "The vpc_id value must be a valid VPC id, starting with \"vpc-\""
+  }
 }
--- modules/subnet/variables.tf
+++ modules/subnet/variables.tf
@@ -10,6 +10,11 @@ variable "subnet_amount" {
 variable "vpc_id" {
   description = "The ID of the VPC"
   type        = string
+
+  validation {
+    condition     = can(regex("^vpc-", var.vpc_id))
+    error_message = "The vpc_id value must be a valid VPC id, starting with \"vpc-\""
+  }
 }
 
 variable "public_subnet_cidr_blocks" {
