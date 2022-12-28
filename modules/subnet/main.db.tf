807aa45c989dc11f91b98957b288adc64b05ab37
Ihor Naidonov <superihor20@gmail.com>
2022-12-28 14:05:55 +0200

    added subnet group for db

---
+++ modules/subnet/main.db.tf
@@ -0,0 +1,9 @@
+resource "aws_db_subnet_group" "db_sg" {
+  name        = "db_subnet_group"
+  description = "DB subnet group for private subnet groups"
+  subnet_ids = [for subnet in aws_subnet.private_subnet : subnet.id]
+
+  depends_on = [
+    aws_subnet.private_subnet
+  ]
+}
